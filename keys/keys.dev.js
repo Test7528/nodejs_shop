b3e14d4e355a0a0abc1e7898cffbdb2e4e93492a
Dilun7525 <#net7525git>
2020-07-10 10:27:41 +0700

    After completing the course

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB_URI:  `mongodb+srv://dilun:2A4NjUHSmwDK8onE@nodejs-course-iswsu.mongodb.net/shop`,
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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',
+  EMAIL_FROM: 'nodejs@email.ru',
+  BASE_URL: 'http://localhost:3000'
+}
